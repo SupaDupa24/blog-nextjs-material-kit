1f5489a6571376be67815866687007711fb73385
DeQuan Sanders <dequansanders17@gmail.com>
2022-04-25 14:37:51 -0400

    add font
    not applied yet

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link href="https://fonts.googleapis.com/css2?family=Six+Caps&display=swap" rel="stylesheet"link />
+          <link href="//db.onlinewebfonts.com/c/20143c892228cf6a5a1510b56d583dca?family=Exocet" rel="stylesheet" type="text/css"/>
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5348558113849875"
      crossorigin="anonymous"></script> 
 
