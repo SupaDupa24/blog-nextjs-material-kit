22b6d0ab8f2349061eed7a87deaca425525388a3
SupaDupa24 <62140687+SupaDupa24@users.noreply.github.com>
2022-08-10 11:14:18 -0500

    Update music.js
    latest song

--- pages/music.js
+++ pages/music.js
@@ -55,6 +55,8 @@ export default function Components(props) {
 
       <div className={classNames(classes.main, classes.mainRaised)}>
         <div className={classes.container}>
+          <iframe src="https://open.spotify.com/embed/track/14h5ljJ9OqVh4Vpl9kFWTV?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+          <br />
           <iframe src="https://open.spotify.com/embed/track/10pQyQqkMQ1c6Qr4G3ucmR?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
           <br />
           <iframe  src="https://open.spotify.com/embed/track/1teXmB63UM12MHIo7nDyqR?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
